cc874544f24a63c5708144bcea809f95303f2c7f
Andrew Le <s3820098@rmit.edu.vn>
2021-12-22 13:58:25 +0700

    13. Render all filter properties for searching

--- components/SearchFilters.js
+++ components/SearchFilters.js
@@ -1,9 +1,46 @@
 import { useEffect, useState } from 'react';
-import { Flex, Select, Box, Text, Input, Spinner, Icon, Button } from '@chakra-ui/react';
+import {
+  Flex,
+  Select,
+  Box,
+  Text,
+  Input,
+  Spinner,
+  Icon,
+  Button,
+} from '@chakra-ui/react';
 import { useRouter } from 'next/router';
 import { MdCancel } from 'react-icons/md';
 import Image from 'next/image';
+import { filterData } from '../utils/filterData';
 
-const SearchFilters = () => <div>Search!!</div>
+const SearchFilters = () => {
+  const [filters] = useState(filterData);
+
+  const searchProperties = (filterValues) => {};
+
+  return (
+    <Flex bg="gray.100" p="4" justifyContent="center" flexWrap="wrap">
+      {filters?.map((filter) => (
+        <Box key={filter.queryName}>
+          <Select
+            onChange={(e) =>
+              searchProperties({ [filter.queryName]: e.target.value })
+            }
+            placeholder={filter.placeholder}
+            w="fit-content"
+            p="2"
+          >
+            {filter?.items?.map((item) => (
+              <option value={item.value} key={item.value}>
+                {item.name}
+              </option>
+            ))}
+          </Select>
+        </Box>
+      ))}
+    </Flex>
+  );
+};
 
 export default SearchFilters;
